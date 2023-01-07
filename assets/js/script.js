42940ce12d0c4d45e544db94224d7b548c976fbf
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-01-08 01:08:32 +0300

    Fixed Navbar responsiveness and bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 'use strict';
 
-// Adding event listener to multiple items
-const addEventonElements = function(elements, eventType, callback){
-    for( let i=0, len=elements.length; i<len; i++){
-        elements[i].addEventListener(eventType,callback);
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
     }
 }
 
 
 
-// PRE LOADER
+/**
+ * PRELOADER
+ */
+
 const preloader = document.querySelector("[data-preloader]");
 
 window.addEventListener("DOMContentLoaded", function () {
@@ -17,29 +25,38 @@ window.addEventListener("DOMContentLoaded", function () {
     document.body.classList.add("loaded");
 });
 
-// NAVBAR
-// Navbar toggle action for mobile size
-const navTogglers = document.querySelectorAll("[data-nav-toggle]");
-const navToggleBtn = document.querySelectorAll("[data-nav-toggle-btn]");
-const navbar = document.querySelectorAll("[data-navbar]");
-const overlay = document.querySelectorAll("[data-overlay]");
 
-const toggleNavbar = function() {
+
+/**
+ * NAVBAR
+ * navbar toggle for mobile
+ */
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
     navbar.classList.toggle("active");
     navToggleBtn.classList.toggle("active");
     overlay.classList.toggle("active");
     document.body.classList.toggle("nav-active");
 }
 
-addEventOnElements(navTogglers,"click", toggleNavbar);
+addEventOnElements(navTogglers, "click", toggleNavbar);
 
-// HEADER
-//  header active when window scroll down to 100px
+
+
+/**
+ * HEADER
+ * header active when window scroll down to 100px
+ */
 
 const header = document.querySelector("[data-header]");
 
-window.addEventListener("scroll", function() {
-    if(window.scrollY >= 100){
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 100) {
         header.classList.add("active");
     } else {
         header.classList.remove("active");

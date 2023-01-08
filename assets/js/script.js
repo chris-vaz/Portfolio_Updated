09235887af3ff77c22f480320d6d168ee72f9f32
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-01-08 16:34:24 +0300

    Updated Content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
+/**
+ * use strict is a directive that tells us the JS engine to enable
+ * strict mode. it helps us write safer code bby detecting and throwing errors
+ * for certain patterns that may be error prone
+ * 
+ * example, in strict mode you can place the string "use strict" at the beginning of a javascript
+ * file or at the beginning of a function block. 
+ * 
+ * strict mode can be used at the beginning of a JS file or at the 
+ * beginning of a function block
+ */
 'use strict';
-
-
 
 /**
  * add event listener on multiple elements
@@ -11,8 +20,6 @@ const addEventOnElements = function (elements, eventType, callback) {
         elements[i].addEventListener(eventType, callback);
     }
 }
-
-
 
 /**
  * PRELOADER
@@ -62,3 +69,88 @@ window.addEventListener("scroll", function () {
         header.classList.remove("active");
     }
 });
+
+
+
+/**
+ * SLIDER
+ */
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const initSlider = function (currentSlider) {
+
+    const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+    const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+    const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+    let totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+    let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    let currentSlidePos = 0;
+
+    const moveSliderItem = function () {
+        sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+    }
+
+    /**
+     * NEXT SLIDE
+     */
+    const slideNext = function () {
+        const slideEnd = currentSlidePos >= totalSlidableItems;
+
+        if (slideEnd) {
+            currentSlidePos = 0;
+        } else {
+            currentSlidePos++;
+        }
+
+        moveSliderItem();
+    }
+
+    sliderNextBtn.addEventListener("click", slideNext);
+
+    /**
+     * PREVIOUS SLIDE
+     */
+    const slidePrev = function () {
+        if (currentSlidePos <= 0) {
+            currentSlidePos = totalSlidableItems;
+        } else {
+            currentSlidePos--;
+        }
+
+        moveSliderItem();
+    }
+
+    sliderPrevBtn.addEventListener("click", slidePrev);
+
+    const dontHaveExtraItem = totalSlidableItems <= 0;
+    if (dontHaveExtraItem) {
+        sliderNextBtn.style.display = 'none';
+        sliderPrevBtn.style.display = 'none';
+    }
+
+    /**
+     * slide with [shift + mouse wheel]
+     */
+
+    currentSlider.addEventListener("wheel", function (event) {
+        if (event.shiftKey && event.deltaY > 0) slideNext();
+        if (event.shiftKey && event.deltaY < 0) slidePrev();
+    });
+
+    /**
+     * RESPONSIVE
+     */
+
+    window.addEventListener("resize", function () {
+        totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
+        totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+        moveSliderItem();
+    });
+
+}
+
+for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
